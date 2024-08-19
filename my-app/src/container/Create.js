3843351d4adafeb826c008d2638519a2ff762702
liu01725 <jeanliu00000@gmail.com>
2024-08-19 18:00:34 -0400

    Wardrobe_Wizard

---
+++ my-app/src/container/Create.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Create.css";
+import OutfitBuiler from "./CreateGrid";
+
+
+export default function Create() {
+
+    return (
+        <div className='crt-outfit'>
+            <OutfitBuiler/>
+        </div>
+    );
+}
